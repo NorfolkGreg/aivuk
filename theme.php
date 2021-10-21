cd137a1b760559f898105f51a4abd89914783120
NorfolkGreg <89905013+NorfolkGreg@users.noreply.github.com>
2021-10-21 21:47:10 +0100

    Add files via upload

--- theme.php
+++ theme.php
@@ -1,11 +1,11 @@
 <?php global $Wcms ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">	<meta http-equiv="X-UA-Compatible" content="IE=edge">	<meta name="viewport" content="width=device-width, initial-scale=1">	<meta name="title" content="<?= $Wcms->get('config', 'siteTitle') ?> - <?= $Wcms->page('title') ?>" />
-	<meta name="description" content="<?= $Wcms->page('description') ?>">	<meta name="keywords" content="<?= $Wcms->page('keywords') ?>">	<title>
+<head>	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">	<meta http-equiv="X-UA-Compatible" content="IE=edge">	<meta name="viewport" content="width=device-width, initial-scale=1">	<meta name="title" content="<?= $Wcms->get('config', 'siteTitle') ?> - <?= $Wcms->page('title') ?>" />	<meta name="description" content="<?= $Wcms->page('description') ?>">	<meta name="keywords" content="<?= $Wcms->page('keywords') ?>">	<title>
 		<?= $Wcms->get('config', 'siteTitle') ?> - <?= $Wcms->page('title') ?>
 	</title>	<link rel="stylesheet" rel="preload" as="style"
-		href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 		integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+		href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+		integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
 		crossorigin="anonymous">	<link rel="stylesheet" rel="preload" as="style"
 		href="<?= $Wcms->asset('css/style.css') ?>">	<?= $Wcms->css() ?></head>
 <body>	<div id="wrapper">
